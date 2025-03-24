4a5ec249f8c038c6c1f9c948864848dcde5f007b
Maria-Farooq534 <146058229+Maria-Farooq534@users.noreply.github.com>
2025-03-24 21:08:23 +0500

    File updated

--- Python-Functions.py
+++ Python-Functions.py
@@ -4,5 +4,14 @@ def user_greeting():
     print("Hi there!")
     print("Welcome!")
     
-
+    
 user_greeting()
+
+#Calling Function with parameter
+
+def user_greeting(user):
+    print("Hi" , user)
+    print("Welcome!")
+    
+    
+user_greeting("Maria")
