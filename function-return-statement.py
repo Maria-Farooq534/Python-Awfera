0ed9f10f11fb548c6111b30e235f208c794b9bd2
Maria-Farooq534 <146058229+Maria-Farooq534@users.noreply.github.com>
2025-03-25 10:05:34 +0500

    File updated

--- function-return-statement.py
+++ function-return-statement.py
@@ -1,5 +1,6 @@
 def calculate_square(num):
-    return num*num
+    #print(num*num) This will return none, coz every function return none by default .
+    return num* num # use return statement 
 
 
 print(calculate_square(2))
