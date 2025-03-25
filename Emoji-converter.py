61c2195ad5f597f24f65663754a3d03b5ca31902
Maria-Farooq534 <146058229+Maria-Farooq534@users.noreply.github.com>
2025-03-25 10:21:04 +0500

    File updated

--- Emoji-converter.py
+++ Emoji-converter.py
@@ -9,3 +9,21 @@ output = ""
 for word in words:
     output += emojis.get(word , word)  + " "
 print(output)
+
+# using Function
+
+
+def emoji_converter(message):
+    splitted_words = message.split(" ")
+    emoji = {
+    ":)" : "😊",
+    ":(" : "😒"
+}
+    display_output = ''
+    for each_word in splitted_words:
+        display_output = display_output + emoji.get(each_word , each_word) + " "
+    return display_output
+
+message = input("> ")
+print(emoji_converter(message))
+        
