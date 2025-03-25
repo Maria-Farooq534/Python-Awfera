3b0b9384bcfeed54cea4e898745223f93db51de8
Maria-Farooq534 <146058229+Maria-Farooq534@users.noreply.github.com>
2025-03-25 09:55:07 +0500

    File updated

--- Python-Functions.py
+++ Python-Functions.py
@@ -31,6 +31,11 @@ def person_name(first_name , last_name):
 person_name("Maria" , "Farooq") #What if we give "Farooq" as first argument and then "Maria" as 2nd.
 person_name("Farooq" , "Maria") #its output is: Hi Farooq Maria .. to avoid this, 
 #Use keyword arguments
-person_name(last_name="Farooq" ,first_name="Maria") #we have to use it for all arguments
+person_name(last_name="Farooq" ,first_name="Maria") 
 # calc_cost(total=50, shipping=5, discount=1)
 #In this example , It helps to understand that what actually each value represents.
+
+#If you're using both positional arguments and keywords argument, use positional argument first, 
+# otherwise python will raise error.
+#person_name(last_name="Farooq" , "Nimra") This will raise syntax error
+person_name("Maria" , last_name="Farooq")
